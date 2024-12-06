a479129d592e3c120c8396d62c522f35558f5e8e
Jorge Cutruzzola <jorgecutruzzola@gmail.com>
2024-12-06 02:34:55 -0300

    post/get mocked pets and users

--- src/controllers/mocks.controller.js
+++ src/controllers/mocks.controller.js
@@ -2,68 +2,73 @@ import MockingService from "../services/mocking.js";
 import { petsService, usersService } from "../services/index.js";
 
 
-const getMockingPets = async (req, res) => {
-    const pets = await MockingService.generateMockingPets(50);
-    res.send({status: "success", payload: pets});
-   
-}
+    const getMockingPets = async (req, res) => {
+        const pets = await MockingService.generateMockingPets(50);
+        res.send({ status: "success", payload: pets });
 
-const getMockingUsers = async (req, res) => {
-    const users = await MockingService.generateMockingUsers(50);
-    res.send({status: "Success", payload: users});
-}
-
-const generateData = async (req, res) => {
-    const {amountPets = 1, amountUsers = 1 } = req.body
-
-    try {
-
-         await MockingService.generateData(amountPets, amountUsers);
-
-        res.json({status: "success", message:"Data generated"});
-
-    } catch (error) {
-        console.error("Error generating data:", error);
-        res.status(500).send("No se pudo generar la data");
-    }
- 
-}
-
-const getMockedUsers = async (req, res) => {
-
-    try {
-         const mockedusers = await usersService.getAll();
-         res.json(mockedusers);
-    
-
-    } catch (error) {
-
-        res.status(500).send("Internal server error")
-    }
-}
-
-const getMockedPets = async (req, res) => {
-    
-    try {
-        
-        const mockedPets = await petsService.getAll();
-        res.json(mockedPets);
-
-    } catch (error) {
-
-        res.status(500).send("Internal server error")
     }
 
-}
-   
+    const getMockingUsers = async (req, res) => {
+        const users = await MockingService.generateMockingUsers(50);
+        res.send({ status: "Success", payload: users });
+    }
 
-export default {
-    getMockingPets, 
-    getMockingUsers,
-    generateData,
-    getMockedPets,
-    getMockedUsers
-}
+    const generateData = async (req, res) => {
+        const { amountPets = 1, amountUsers = 1 } = req.body;
+
+        try {
+            
+            await MockingService.generateData(amountPets, amountUsers);
+
+            res.json({ status: "success", message: "Data generated" });
+
+        } catch (error) {
+            console.error("Error generating data:", error);
+            
+            res.status(500).send("Internal server error");
+        }
+
+    }
+
+    const getMockedUsers = async (req, res) => {
+
+        try {
+            const mockedusers = await usersService.getAll();
+
+            if(!mockedusers) return res.status(404).send("Error al traer mocked users");
+
+            res.json(mockedusers);
+
+
+        } catch (error) {
+            res.status(500).send("Internal server error")
+        }
+    }
+
+    const getMockedPets = async (req, res) => {
+
+        try {
+
+            const mockedPets = await petsService.getAll();
+
+            if(!mockedPets) return res.status(404).send("Error al traer mocked pets")
+
+            res.json(mockedPets);
+
+        } catch (error) {
+            res.status(500).send("Internal server error")
+        }
+
+    }
+
+
+    export default {
+        getMockingPets,
+        getMockingUsers,
+        generateData,
+        getMockedPets,
+        getMockedUsers
+    }
 
 
 // Function para la ruta /generatedata pero solo funciona con metodo GET ya que debe enviar la data a la db y devolverla al cliente \\
